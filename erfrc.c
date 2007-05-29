2e2684a3efb06b6a59b1434340ec40e9bd0476d3
yang <yang@9d4abd89-f43f-0410-b58a-db57313e21a5>
2007-05-29 22:23:38 +0000

    Still a test for CVS of capturing file changes.

--- erfrc.c
+++ erfrc.c
@@ -1,3 +1,7 @@
+/* Calculate inter atom and inter molecule energies and forces.
+   These include different atoms on the same molecule (exclude 1-2,1-3)
+   and atoms on different molecules.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
