d1a597589a264c8482c9b768a907797f61a92665
ywangd <ywangd@9d4abd89-f43f-0410-b58a-db57313e21a5>
2007-11-30 07:46:52 +0000

    Created a new file, md.c, and separate the MD related code from main() into it.

---
+++ md.c
@@ -0,0 +1,160 @@
+/*
+ * Functions related to MD operation go here
+ *
+ * Written by Yang Wang 11-29-2007
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <ctype.h>
+#include "vars.h"
+
+extern int vver();
+extern int erfrc();
+extern int rafrc();
+extern int vver_nh_3();
+extern int npt_respa();
+extern int averages();
+extern int loadit();
+extern int saveit();
+extern int printit();
+extern int snapshot();
+extern int trajectory();
+extern int velinit();
+extern int echo();
+
+int do_accumu()
+{
+    // accumulators
+    accumulator[0][0] += utot;
+    accumulator[0][1] += utot*utot;
+    accumulator[1][0] += upot;
+    accumulator[1][1] += upot*upot;
+    accumulator[2][0] += ukin;
+    accumulator[2][1] += ukin*upot;
+    accumulator[3][0] += uinter;
+    accumulator[3][1] += uinter*uinter;
+    accumulator[4][0] += uintra;
+    accumulator[4][1] += uintra*uintra;
+    accumulator[5][0] += uvdw;
+    accumulator[5][1] += uvdw*uvdw;
+    accumulator[6][0] += ubond;
+    accumulator[6][1] += ubond*ubond;
+    accumulator[7][0] += uangle;
+    accumulator[7][1] += uangle*uangle;
+    accumulator[8][0] += udih;
+    accumulator[8][1] += udih*udih;
+    accumulator[9][0] += uimp;
+    accumulator[9][1] += uimp*uimp;
+    accumulator[10][0] += uewald;
+    accumulator[10][1] += uewald*uewald;
+    accumulator[11][0] += ureal;
+    accumulator[11][1] += ureal*ureal;
+    accumulator[12][0] += ufourier;
+    accumulator[12][1] += ufourier*ufourier;
+    accumulator[13][0] += uself;
+    accumulator[13][1] += uself*uself;
+    accumulator[14][0] += usflj;
+    accumulator[14][1] += usflj*usflj;
+    accumulator[15][0] += tinst;
+    accumulator[15][1] += tinst*tinst;
+    accumulator[16][0] += uvacuum;
+    accumulator[16][1] += uvacuum*uvacuum;
+    accumulator[17][0] += uwolf;
+    accumulator[17][1] += uwolf*uwolf;
+    accumulator[18][0] += pinst;
+    accumulator[18][1] += pinst*pinst;
+    accumulator[19][0] += boxv;
+    accumulator[19][1] += boxv*boxv;
+    accumulator[20][0] += pideal;
+    accumulator[20][1] += pideal*pideal;
+
+    return 0;
+}
+
+int md ()
+{
+    // start of the MD simulation
+    // initialize velocities
+    velinit(); 
+
+    // if not new run, load from old file
+    if (fStart_option!=new_run) loadit(); 
+
+    // calculate total energies
+    erfrc(); 
+    rafrc();
+    // print out initial values
+    echo();
+    // print initial properties
+    printit();
+    // make snapshots & movies
+    trajectory();
+
+    if (what_ensemble == nvt_run) // velocity verlet with nose hoover for NVT MD
+    {
+	for (istep=nstep_start;istep<=nstep;istep++) // NOTE: start from 1 and <=
+	{	
+	    vver_nh_3();
+	    // print out, snapshot, trajectory, save
+	    if (istep%nstep_print == 0) printit();
+	    if (nstep_ss && istep%nstep_ss == 0) snapshot();
+	    if (nstep_trj && istep%nstep_trj==0) trajectory();
+	    if (istep%nstep_save==0) saveit();
+
+	    icounter[11]--;
+	    // if still in equilibrium run
+	    // do not do averages
+	    if (icounter[11]>=0) continue;
+	    // accumulators
+	    do_accumu();
+	    if ((istep-nstep_eq)%nstep_ave==0) averages();
+	}
+    }
+    else if (what_ensemble == npt_run) // baronstat for NPT MD
+    {
+	for (istep=nstep_start;istep<=nstep;istep++) // NOTE: start from 1 and <=
+	{	
+	    npt_respa();
+	    // print out, snapshot, trajectory, save
+	    if (istep%nstep_print == 0) printit();
+	    if (nstep_ss && istep%nstep_ss == 0) snapshot();
+	    if (nstep_trj && istep%nstep_trj==0) trajectory();
+	    if (istep%nstep_save==0) saveit();
+
+	    icounter[11]--;
+	    // if still in equilibrium run
+	    // do not do averages
+	    if (icounter[11]>=0) continue;
+	    // accumulators
+	    do_accumu();
+	    if ((istep-nstep_eq)%nstep_ave==0) averages();
+	}
+    }
+    else // velocity verlet for normal NVE MD
+    {
+	for (istep=nstep_start;istep<=nstep;istep++) // NOTE: start from 1 and <=
+	{	
+	    vver(); 
+	    // print out, snapshot, trajectory, save
+	    if (istep%nstep_print == 0) printit();
+	    if (nstep_ss && istep%nstep_ss == 0) snapshot();
+	    if (nstep_trj && istep%nstep_trj==0) trajectory();
+	    if (istep%nstep_save==0) saveit();
+
+	    icounter[11]--;
+	    // if still in equilibrium run
+	    // do not do averages
+	    if (icounter[11]>=0) continue;
+	    // accumulators
+	    do_accumu();
+	    if ((istep-nstep_eq)%nstep_ave==0) averages();
+	}
+    }
+
+    return 0;
+}
+
--- mspms2.c
+++ mspms2.c
@@ -25,16 +25,13 @@
 
 extern int erfrc();
 extern int rafrc();
-extern int vver_nh_1();
-extern int vver_nh_2();
-extern int vver_nh_3();
 extern int init_sf_hypergeo();
 extern int init_sf_atom_explicit();
 extern int init_tasos_grid();
 extern int init_my_interp();
 extern int init_npt_respa();
 extern int init_nvt();
-extern int npt_respa();
+extern int md();
 
 
 int calculate_ljlrc();
@@ -1392,7 +1389,6 @@ int opening()
     fprintf(fpouts,"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWYWANG\n");
 }
 
-extern int get_values_from_grid(double, double, double, int, double*, double*);
 int main (int argc, char *argv[])
 {
     opening();
@@ -1400,110 +1396,8 @@ int main (int argc, char *argv[])
     init_vars();
     make_exclude_list();
 
-    // start of the MD simulation
-    // initialize velocities
-    velinit(); 
-
-    if (fStart_option!=new_run) loadit(); // if not new run, load from old file
-
-    // calculate total energies
-    erfrc(); 
-    rafrc();
-    // print out initial values
-    echo();
-    // print initial properties
-    printit();
-    // make snapshots & movies
-    trajectory();
-
-    for (istep=nstep_start;istep<=nstep;istep++) // NOTE: start from 1 and <=
-    {
-	if (what_ensemble == nvt_run) // velocity verlet with nose hoover for NVT
-	{
-	    vver_nh_3();
-	    /*
-	    switch (whichNH)
-	    {
-		case 1:
-		    vver_nh_1();
-		    break;
-		case 2:
-		    vver_nh_2();
-		    break;
-		case 3:
-		    vver_nh_3();
-		    break;
-	    }	
-	    */
-	}
-	else if (what_ensemble == npt_run)
-	{
-	    npt_respa();
-	}
-	else 
-	    vver(); // velocity verlet
-
-	// print out, snapshot, trajectory, save
-	if (istep%nstep_print == 0) printit();
-
-	if (nstep_ss && istep%nstep_ss == 0) snapshot();
-
-	if (nstep_trj && istep%nstep_trj==0) trajectory();
-
-	if (istep%nstep_save==0) saveit();
-
-	icounter[11]--;
-	// if still in equilibrium run
-	// do not do averages
-	if (icounter[11]>=0) continue;
-
-	// accumulators
-	accumulator[0][0] += utot;
-	accumulator[0][1] += utot*utot;
-	accumulator[1][0] += upot;
-	accumulator[1][1] += upot*upot;
-	accumulator[2][0] += ukin;
-	accumulator[2][1] += ukin*upot;
-	accumulator[3][0] += uinter;
-	accumulator[3][1] += uinter*uinter;
-	accumulator[4][0] += uintra;
-	accumulator[4][1] += uintra*uintra;
-	accumulator[5][0] += uvdw;
-	accumulator[5][1] += uvdw*uvdw;
-	accumulator[6][0] += ubond;
-	accumulator[6][1] += ubond*ubond;
-	accumulator[7][0] += uangle;
-	accumulator[7][1] += uangle*uangle;
-	accumulator[8][0] += udih;
-	accumulator[8][1] += udih*udih;
-	accumulator[9][0] += uimp;
-	accumulator[9][1] += uimp*uimp;
-	accumulator[10][0] += uewald;
-	accumulator[10][1] += uewald*uewald;
-	accumulator[11][0] += ureal;
-	accumulator[11][1] += ureal*ureal;
-	accumulator[12][0] += ufourier;
-	accumulator[12][1] += ufourier*ufourier;
-	accumulator[13][0] += uself;
-	accumulator[13][1] += uself*uself;
-	accumulator[14][0] += usflj;
-	accumulator[14][1] += usflj*usflj;
-	accumulator[15][0] += tinst;
-	accumulator[15][1] += tinst*tinst;
-	accumulator[16][0] += uvacuum;
-	accumulator[16][1] += uvacuum*uvacuum;
-	accumulator[17][0] += uwolf;
-	accumulator[17][1] += uwolf*uwolf;
-	accumulator[18][0] += pinst;
-	accumulator[18][1] += pinst*pinst;
-	accumulator[19][0] += boxv;
-	accumulator[19][1] += boxv*boxv;
-	accumulator[20][0] += pideal;
-	accumulator[20][1] += pideal*pideal;
-
-	if ((istep-nstep_eq)%nstep_ave==0) averages();
-
-    } // loop through all simulation time
+    // MD simulation
+    md();
 
     snapshot();
 
