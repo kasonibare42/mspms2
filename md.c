47f406998990331b76ead7b72adf1187f149edbd
ywangd <ywangd@9d4abd89-f43f-0410-b58a-db57313e21a5>
2007-12-08 05:50:18 +0000

    Moved vver() to md.c from mspms2.c

--- md.c
+++ md.c
@@ -12,7 +12,6 @@
 #include <ctype.h>
 #include "vars.h"
 
-extern int vver();
 extern int erfrc();
 extern int rafrc();
 extern int vver_nh_3();
@@ -25,6 +24,63 @@ extern int snapshot();
 extern int trajectory();
 extern int velinit();
 extern int echo();
+
+int vver() // velocity verlet
+{
+    int ii, ll;
+
+    for (ii=0;ii<natom;ii++)
+    {
+		// the factor of 1.0e-5 is based on Angstrom (from the force)
+		// and femto second (from delt)
+		vx[ii] += (deltby2*fxl[ii]*1.0e-5/aw[ii]);
+		vy[ii] += (deltby2*fyl[ii]*1.0e-5/aw[ii]);
+		vz[ii] += (deltby2*fzl[ii]*1.0e-5/aw[ii]);
+    }
+
+    for (ll=0;ll<nstep_inner;ll++)
+    {
+		for (ii=0;ii<natom;ii++)
+		{
+		    vx[ii] += (deltsby2*fxs[ii]*1.0e-5/aw[ii]);
+		    vy[ii] += (deltsby2*fys[ii]*1.0e-5/aw[ii]);
+		    vz[ii] += (deltsby2*fzs[ii]*1.0e-5/aw[ii]);
+		    xx[ii] = xx[ii] + delts*vx[ii]*1.0e-5;
+		    yy[ii] = yy[ii] + delts*vy[ii]*1.0e-5;
+		    zz[ii] = zz[ii] + delts*vz[ii]*1.0e-5;
+		}
+	
+		// intra forces, short ranged
+		rafrc();
+	
+		// compute the pseudo velocity at delts
+		for (ii=0;ii<natom;ii++)
+		{
+		    vx[ii] += (deltsby2*fxs[ii]*1.0e-5/aw[ii]);
+		    vy[ii] += (deltsby2*fys[ii]*1.0e-5/aw[ii]);
+		    vz[ii] += (deltsby2*fzs[ii]*1.0e-5/aw[ii]);
+		}
+    }
+
+    // inter forces, long ranged
+    erfrc();
+
+    // use the new forces to calculate the new velocities at t+delt
+    ukin = 0.0;
+    for (ii=0;ii<natom;ii++)
+    {
+		vx[ii] += (deltby2*fxl[ii]*1.0e-5/aw[ii]);
+		vy[ii] += (deltby2*fyl[ii]*1.0e-5/aw[ii]);
+		vz[ii] += (deltby2*fzl[ii]*1.0e-5/aw[ii]);
+		ukin += aw[ii]*(vx[ii]*vx[ii]+vy[ii]*vy[ii]+vz[ii]*vz[ii]);
+    }
+    ukin = ukin/2.0;
+
+    // calculate instant temperature
+    tinst = 2.0*ukin*rRgas/nfree;
+    
+    return 0;
+}
 
 int do_accumu()
 {
--- mspms2.c
+++ mspms2.c
@@ -23,8 +23,6 @@
 #include "vars.h"
 #include "random.h"
 
-extern int erfrc();
-extern int rafrc();
 extern int init_sf_hypergeo();
 extern int init_sf_atom_explicit();
 extern int init_tasos_grid();
@@ -1177,63 +1175,6 @@ int printit()
 
     fprintf(fplog,"%10.4le %10.4le %10.4le\n",
 	    utsbs,pinst,boxv);
-    
-    return 0;
-}
-
-int vver() // velocity verlet
-{
-    int ii, ll;
-
-    for (ii=0;ii<natom;ii++)
-    {
-		// the factor of 1.0e-5 is based on Angstrom (from the force)
-		// and femto second (from delt)
-		vx[ii] += (deltby2*fxl[ii]*1.0e-5/aw[ii]);
-		vy[ii] += (deltby2*fyl[ii]*1.0e-5/aw[ii]);
-		vz[ii] += (deltby2*fzl[ii]*1.0e-5/aw[ii]);
-    }
-
-    for (ll=0;ll<nstep_inner;ll++)
-    {
-		for (ii=0;ii<natom;ii++)
-		{
-		    vx[ii] += (deltsby2*fxs[ii]*1.0e-5/aw[ii]);
-		    vy[ii] += (deltsby2*fys[ii]*1.0e-5/aw[ii]);
-		    vz[ii] += (deltsby2*fzs[ii]*1.0e-5/aw[ii]);
-		    xx[ii] = xx[ii] + delts*vx[ii]*1.0e-5;
-		    yy[ii] = yy[ii] + delts*vy[ii]*1.0e-5;
-		    zz[ii] = zz[ii] + delts*vz[ii]*1.0e-5;
-		}
-	
-		// intra forces, short ranged
-		rafrc();
-	
-		// compute the pseudo velocity at delts
-		for (ii=0;ii<natom;ii++)
-		{
-		    vx[ii] += (deltsby2*fxs[ii]*1.0e-5/aw[ii]);
-		    vy[ii] += (deltsby2*fys[ii]*1.0e-5/aw[ii]);
-		    vz[ii] += (deltsby2*fzs[ii]*1.0e-5/aw[ii]);
-		}
-    }
-
-    // inter forces, long ranged
-    erfrc();
-
-    // use the new forces to calculate the new velocities at t+delt
-    ukin = 0.0;
-    for (ii=0;ii<natom;ii++)
-    {
-		vx[ii] += (deltby2*fxl[ii]*1.0e-5/aw[ii]);
-		vy[ii] += (deltby2*fyl[ii]*1.0e-5/aw[ii]);
-		vz[ii] += (deltby2*fzl[ii]*1.0e-5/aw[ii]);
-		ukin += aw[ii]*(vx[ii]*vx[ii]+vy[ii]*vy[ii]+vz[ii]*vz[ii]);
-    }
-    ukin = ukin/2.0;
-
-    // calculate instant temperature
-    tinst = 2.0*ukin*rRgas/nfree;
     
     return 0;
 }
